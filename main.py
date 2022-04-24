703ddc9ca3d1bf5f6f2643833cdb2f8547b77ddc
Quyen Linh TA <linhtq.sfu@gmail.com>
2022-04-24 14:45:19 +0200

    Update : Masking Process

--- main.py
+++ main.py
@@ -9,23 +9,23 @@ examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en', with_info=True,
 train_examples, val_examples = examples['train'], examples['validation']
 
 tokenizer_en = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
-    (en.numpy() for pt, en in train_examples), target_vocab_size=2**13)
+    (en.numpy() for pt, en in train_examples), target_vocab_size=2 ** 13)
 
 tokenizer_pt = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
-    (pt.numpy() for pt, en in train_examples), target_vocab_size=2**13)
+    (pt.numpy() for pt, en in train_examples), target_vocab_size=2 ** 13)
 
 sample_string = 'Transformer is awesome.'
 
 tokenized_string = tokenizer_en.encode(sample_string)
-print ('Tokenized string is {}'.format(tokenized_string))
+print('Tokenized string is {}'.format(tokenized_string))
 
 original_string = tokenizer_en.decode(tokenized_string)
-print ('The original string: {}'.format(original_string))
+print('The original string: {}'.format(original_string))
 
 assert original_string == sample_string
 
 for ts in tokenized_string:
-  print ('{} ----> {}'.format(ts, tokenizer_en.decode([ts])))
+    print('{} ----> {}'.format(ts, tokenizer_en.decode([ts])))
 
 BUFFER_SIZE = 20000
 BATCH_SIZE = 64
@@ -40,18 +40,22 @@ def encode(lang1, lang2):
 
     return lang1, lang2
 
-def tf_encode(pt, en):
-  result_pt, result_en = tf.py_function(encode, [pt, en], [tf.int64, tf.int64])
-  result_pt.set_shape([None])
-  result_en.set_shape([None])
 
-  return result_pt, result_en
+def tf_encode(pt, en):
+    result_pt, result_en = tf.py_function(encode, [pt, en], [tf.int64, tf.int64])
+    result_pt.set_shape([None])
+    result_en.set_shape([None])
+
+    return result_pt, result_en
+
 
 MAX_LENGTH = 40
 
+
 def filter_max_length(x, y, max_length=MAX_LENGTH):
-  return tf.logical_and(tf.size(x) <= max_length,
-                        tf.size(y) <= max_length)
+    return tf.logical_and(tf.size(x) <= max_length,
+                          tf.size(y) <= max_length)
+
 
 train_dataset = train_examples.map(tf_encode)
 train_dataset = train_dataset.filter(filter_max_length)
@@ -60,16 +64,16 @@ train_dataset = train_dataset.cache()
 train_dataset = train_dataset.shuffle(BUFFER_SIZE).padded_batch(BATCH_SIZE)
 train_dataset = train_dataset.prefetch(tf.data.experimental.AUTOTUNE)
 
-
 val_dataset = val_examples.map(tf_encode)
 val_dataset = val_dataset.filter(filter_max_length).padded_batch(BATCH_SIZE)
 
 pt_batch, en_batch = next(iter(val_dataset))
 pt_batch, en_batch
 
+
 def get_angles(pos, i, d_model):
-  angle_rates = 1 / np.power(10000, (2 * (i//2)) / np.float32(d_model))
-  return pos * angle_rates
+    angle_rates = 1 / np.power(10000, (2 * (i // 2)) / np.float32(d_model))
+    return pos * angle_rates
 
 
 def positional_encoding(position, d_model):
@@ -87,8 +91,9 @@ def positional_encoding(position, d_model):
 
     return tf.cast(pos_encoding, dtype=tf.float32)
 
+
 pos_encoding = positional_encoding(50, 512)
-print (pos_encoding.shape)
+print(pos_encoding.shape)
 
 plt.pcolormesh(pos_encoding[0], cmap='RdBu')
 plt.xlabel('Depth')
@@ -101,3 +106,18 @@ plt.show()
 def create_padding_mask(seq):
     seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
     return seq[:, tf.newaxis, tf.newaxis, :]  # (batch_size, 1, 1, seq_len)
+
+
+x = tf.constant([[7, 6, 0, 0, 1], [1, 2, 3, 0, 0], [0, 0, 0, 4, 5]])
+create_padding_mask(x)
+
+
+def create_look_ahead_mask(size):
+    mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
+    return mask  # (seq_len, seq_len)
+
+
+x = tf.random.uniform((1, 3))
+temp = create_look_ahead_mask(x.shape[1])
+print(temp)
+
